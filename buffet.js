e2053f95ebed0626a301a174845178e4682d11f6
ajaycprakash <ajaycprakash18@gmail.com>
2024-03-18 11:13:45 +0530

    receip

--- buffet.js
+++ buffet.js
@@ -16,7 +16,7 @@
 //     time--;
 // }
 
-const date_time1 = new Date('March 17, 2024 0:02:31').getTime();  
+const date_time1 = new Date('March 17, 2024 04:02:31').getTime();  
 
 var fun = setInterval(function() {  
 
@@ -24,10 +24,20 @@ const date_time2 =  new Date().getTime();
 
 const time = date_time1 - date_time2;  
 
+if (time <= 0) {
+    clearInterval(fun);
+    document.getElementById("day").innerHTML = "00";
+    document.getElementById("hour").innerHTML = "00";
+    document.getElementById("minute").innerHTML = "00";
+    document.getElementById("second").innerHTML = "00";
+    return;
+}
+
 const days = Math.floor(time / (1000 * 60 * 60 * 24));  
 const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));  
 const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));  
-const seconds = Math.floor((time % (1000 * 60)) / 1000);  
+const seconds = Math.floor((time % (1000 * 60)) / 1000);
+ 
 
 document.getElementById("day").innerHTML = days+":";  
 document.getElementById("hour").innerHTML = hours+":";  
